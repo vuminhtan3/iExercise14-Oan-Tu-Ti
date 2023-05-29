d8fea78ef65bce7b5db0a2b802c784bbc726625b
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-05-29 21:48:26 +0700

    Complete design UI

---
+++ OanTuTi/OanTuTi/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  OanTuTi
+//
+//  Created by Minh Tan Vu on 29/05/2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rockButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func paperButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func scissorButtonPressed(_ sender: UIButton) {
+    }
+    
+}
