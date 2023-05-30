7ea002e2011b9c5eb8dad93a9c62b6f268e3a54d
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-05-30 23:07:26 +0700

    Complete logic code with simple UI

--- OanTuTi/OanTuTi/ResultViewController.swift
+++ OanTuTi/OanTuTi/ResultViewController.swift
@@ -9,23 +9,70 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
-    @IBOutlet weak var myImageView: UIImageView!
+    var imageSet = ["rock", "paper", "scissor"]
+    var rockImg = UIImage(named: "rock")
+    var paperImg = UIImage(named: "paper")
+    var scissorImg = UIImage(named: "scissor")
+    var myImg = UIImage()
+    var comImg = UIImage()
+    var comImgName = String()
+    var resultText = String()
     
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var comImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+        updateUI()
     }
     
     @IBAction func rockButtonPressed(_ sender: UIButton) {
+        myImg = rockImg!
+        comImgName = imageSet.randomElement()!
+        comImg = UIImage(named: comImgName)!
+        if comImgName == "rock" {
+            resultText = "DRAW"
+        } else if comImgName == "paper" {
+            resultText = "LOSE"
+        } else {
+            resultText = "WIN"
+        }
+        updateUI()
     }
     
     @IBAction func paperButtonPressed(_ sender: UIButton) {
+        myImg = paperImg!
+        comImgName = imageSet.randomElement()!
+        comImg = UIImage(named: comImgName)!
+        if comImgName == "rock" {
+            resultText = "WIN"
+        } else if comImgName == "paper" {
+            resultText = "DRAW"
+        } else {
+            resultText = "LOSE"
+        }
+        updateUI()
     }
     
     @IBAction func scissorButtonPressed(_ sender: UIButton) {
+        myImg = scissorImg!
+        comImgName = imageSet.randomElement()!
+        comImg = UIImage(named: comImgName)!
+        if comImgName == "rock" {
+            resultText = "LOSE"
+        } else if comImgName == "paper" {
+            resultText = "WIN"
+        } else {
+            resultText = "DRAW"
+        }
+        updateUI()
     }
     
+    func updateUI() {
+        myImageView.image = myImg
+        comImageView.image = comImg
+        resultLabel.text = resultText
+    }
 }
--- OanTuTi/OanTuTi/ViewController.swift
+++ OanTuTi/OanTuTi/ViewController.swift
@@ -14,6 +14,63 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rockButtonPressed(_ sender: UIButton) {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "ResultVC") as! ResultViewController
+        secondVC.modalPresentationStyle = .fullScreen
+        
+        let comImgName = secondVC.imageSet.randomElement()!
+        secondVC.myImg = UIImage(named: "rock")!
+        secondVC.comImg = UIImage(named: comImgName)!
+        if comImgName == "rock" {
+            secondVC.resultText = "DRAW"
+        } else if comImgName == "paper" {
+            secondVC.resultText = "LOSE"
+        } else {
+            secondVC.resultText = "WIN"
+        }
+        
+        self.present(secondVC, animated: true)
+        
+    }
+    
+    @IBAction func paperButtonPressed(_ sender: UIButton) {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "ResultVC") as! ResultViewController
+        secondVC.modalPresentationStyle = .fullScreen
+        
+        let comImgName = secondVC.imageSet.randomElement()!
+        secondVC.myImg = UIImage(named: "paper")!
+        secondVC.comImg = UIImage(named: comImgName)!
+        if comImgName == "rock" {
+            secondVC.resultText = "WIN"
+        } else if comImgName == "paper" {
+            secondVC.resultText = "DRAW"
+        } else {
+            secondVC.resultText = "LOSE"
+        }
+        
+        self.present(secondVC, animated: true)
+    }
+    
+    @IBAction func scissorButtonPressed(_ sender: UIButton) {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "ResultVC") as! ResultViewController
+        secondVC.modalPresentationStyle = .fullScreen
+        
+        let comImgName = secondVC.imageSet.randomElement()!
+        secondVC.myImg = UIImage(named: "scissor")!
+        secondVC.comImg = UIImage(named: comImgName)!
+        if comImgName == "rock" {
+            secondVC.resultText = "LOSE"
+        } else if comImgName == "paper" {
+            secondVC.resultText = "WIN"
+        } else {
+            secondVC.resultText = "DRAW"
+        }
+        
+        self.present(secondVC, animated: true)
+    }
+    
 }
 
